ff1c29925c59edcdfd4c90d1ffae89053828ba0e
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2021-03-27 22:26:49 +0100

    Create AssetBundleHandler.cs

---
+++ src/AssetBundleHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Reactor.Extensions;
+using UnityEngine;
+
+namespace MinatoMod
+{
+    public static class AssetBundleHandler
+    {
+        private static AssetBundle AssetsBundle;
+
+        public static void LoadAssetBundle()
+        {
+            byte[] bundleRead = Assembly.GetCallingAssembly().GetManifestResourceStream("MinatoMod.src.Assets.bundle").ReadFully();
+            AssetsBundle = AssetBundle.LoadFromMemory(bundleRead);
+        }
+
+        public static Object LoadAssetFromBundle(string name)
+        {
+            return AssetsBundle.LoadAsset(name);
+        }
+    }
+}
